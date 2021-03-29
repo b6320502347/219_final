998d6f75c359bcc7539a8b186a90e71bf62764e3
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-29 18:24:04 +0700

    .

--- 6320502347_final_1.c
+++ 6320502347_final_1.c
@@ -1,10 +1,48 @@
 #include<stdio.h>
 int main(){
-    int k,c,n;
+    int k,c,n,i,num;
     scanf("%d %d",&n,&c);
-    for(k=n;;k++)
-        if(k%10==c)
+    if(n>0&&n<32750){
+    int x[5];
+    for(k=n+1;;k++){
+        for(i=0,num=k;num>0;i++)
+        num=num/10;
+    if(i==1){
+        if(k!=1){
+            k+=10;
             break;
+            }
+        else if(k==1){
+            k==10;
+            break;
+            }
+            }
+    else if(i==2){
+        for(num=k,i=2;i>0;i--,num/=10)
+            x[2-i]=num%10;
+        if(c==x[0]||c==x[1])
+            break;
+    }
+    else if(i==3){
+        for(num=k,i=3;i>0;i--,num/=10)
+            x[3-i]=num%10;
+            if(c==x[0]||c==x[1]||c==x[2])
+            break;
+            }
+        else if(i==4){
+        for(num=k,i=4;i>0;i--,num/=10)
+            x[4-i]=num%10;
+            if(c==x[0]||c==x[1]||c==x[2]||c==x[3])
+            break;
+            }
+        else if(i==5){
+        for(num=k,i=5;i>0;i--,num/=10)
+            x[5-i]=num%10;
+            if(c==x[0]||c==x[1]||c==x[2]||c==x[3]||c==x[4])
+            break;
+            }
+    }
     printf("%d",k);
+    }
 
 }
